fb2d45ff5cb8395a14e428a97f031d7f03292399
Mujtaba Basheer <mujtaba.fleapo@gmail.com>
2022-11-28 20:10:33 +0530

    modified .yml file

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ module.exports = () => {
   const nextConfig = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-    env: {},
     images: {
       unoptimized: true,
     },
