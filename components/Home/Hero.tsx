e49df05161b5937aaf221458ce48ef32904a8a09
Mujtaba Basheer <mujtaba.fleapo@gmail.com>
2022-12-01 12:00:32 +0530

    hotfix

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,269 +1,311 @@
 /** @format */
 
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import device from "../../styles/breakpoints";
 import NavBar from "./Navbar";
 
 const asset_prefix = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 const Container = styled.div`
-	display: flex;
-	flex: 0.5;
-	flex-direction: column;
-	justify-content: start;
-	align-content: start;
-	box-sizing: border-box;
+  display: flex;
+  flex: 0.5;
+  flex-direction: column;
+  justify-content: start;
+  align-content: start;
+  box-sizing: border-box;
 
-	@media ${device.mobile} {
-		margin-top: 2em;
-		margin-bottom: 10em;
-	}
+  @media ${device.mobile} {
+    margin-top: 2em;
+    margin-bottom: 10em;
+  }
 `;
 
-const HeaderTag = styled.h1``;
+const HeaderTag = styled.h1`
+  font-weight: 700;
+  color: #ffffff;
+  min-height: 76.66px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-const Trade = styled.span`
-	color: rgba(32, 49, 174, 1);
-	margin-right: 0.3em;
-	font-size: 56px;
-	font-weight: 700;
+const wheel = keyframes`
+    0% {
+        transform: translateY(-100%);
+    }
+
+    50% {
+        transform: translateY(-50%);
+    }
+
+    100% {
+        transform: translateY(0);
+    }
+`;
+
+const AnimContainer = styled.div`
+  width: 100%;
+  height: 200%;
+  transform: translateY(-100%);
+  // animation: ${wheel} 2.5s cubic-bezier(0.7, 0.08, 0.03, 0.82) infinite forwards;
+  animation-delay: 2.5s;
+`;
+
+const AnimText = styled.span`
+  color: rgba(32, 49, 174, 1);
+  margin-right: 0.3em;
+  font-size: 56px;
+  font-weight: 700;
+
+  & span {
+    display: block;
+    height: 50%;
+    top: 0;
+    right: 0;
+  }
 `;
 
 const On = styled.span`
-	color: rgba(255, 255, 255, 1);
-	margin-right: 0.3rem;
-	font-size: 56px;
-	font-weight: 700;
+  color: rgba(255, 255, 255, 1);
+  margin-right: 0.3rem;
+  font-size: 56px;
+  font-weight: 700;
 `;
 
 const Description = styled.p`
-	margin-top: 1rem;
-	font-size: 20px;
-	font-weight: 400;
-	line-height: 28px;
-	color: #15182c;
+  margin-top: 1rem;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 28px;
+  color: #15182c;
 
-	@media ${device.mobile} {
-		font-size: 1rem;
-		text-justify: auto;
-		font-weight: 400;
-		font-size: 16px;
-		line-height: 140%;
-		width: 320px;
-	}
+  @media ${device.mobile} {
+    font-size: 1rem;
+    text-justify: auto;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 140%;
+    width: 320px;
+  }
 `;
 
 const GetLinkArea = styled.div`
-	margin-top: 1.5rem;
-	display: flex;
-	flex-direction: row;
-	min-width: 414px;
+  margin-top: 1.5rem;
+  display: flex;
+  flex-direction: row;
+  min-width: 414px;
 
-	@media ${device.mobile} {
-		display: block;
-	}
+  @media ${device.mobile} {
+    display: block;
+  }
 `;
 
 const InputArea = styled.div`
-	background: #ffffff;
-	border-radius: 30px;
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	padding: 12px 16px;
-	width: 80%;
-	box-sizing: border-box;
+  background: #ffffff;
+  border-radius: 30px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 12px 16px;
+  width: 80%;
+  box-sizing: border-box;
 
-	@media ${device.mobile} {
-		width: 320px;
-		margin-bottom: 1em;
-	}
+  @media ${device.mobile} {
+    width: 320px;
+    margin-bottom: 1em;
+  }
 `;
 
 const NumberSpan = styled.span`
-	height: "100%";
-	margin-right: 10px;
-	font-size: 18px;
-	font-weight: 400px;
+  height: "100%";
+  margin-right: 10px;
+  font-size: 18px;
+  font-weight: 400px;
 `;
 
 const Input = styled.input`
-	height: 150%;
-	flex: 0.7;
-	border: none;
-	font-size: 18px;
-	font-weight: 400px;
-	&:focus {
-		outline: none;
-	}
+  height: 150%;
+  flex: 0.7;
+  border: none;
+  font-size: 18px;
+  font-weight: 400px;
+  &:focus {
+    outline: none;
+  }
 
-	/* padding: 8px 8px; */
+  /* padding: 8px 8px; */
 `;
 
 const LinkButton = styled.button`
-	background: #2031ae;
-	font-size: 16px;
-	border-radius: 47px;
-	position: relative;
-	border: none;
-	height: 50px;
-	right: 80px;
-	top: 0px;
-	color: #fff;
-	width: 250px;
-	min-width: 200px;
-	padding: 15.5px 24px;
-	cursor: pointer;
-	font-family: "Barlow";
-	@media ${device.mobile} {
-		right: 0px;
-		font-size: 1rem;
-		font-weight: 600;
-	}
+  background: #2031ae;
+  font-size: 16px;
+  border-radius: 47px;
+  position: relative;
+  border: none;
+  height: 50px;
+  right: 80px;
+  top: 0px;
+  color: #fff;
+  width: 250px;
+  min-width: 200px;
+  padding: 15.5px 24px;
+  cursor: pointer;
+  font-family: "Barlow";
+  @media ${device.mobile} {
+    right: 0px;
+    font-size: 1rem;
+    font-weight: 600;
+  }
 `;
 
 const StatsArea = styled.div`
-	display: flex;
-	flex-direction: row;
-	justify-content: space-between;
-	margin-top: 4rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 4rem;
 
-	@media ${device.mobile} {
-		width: 320px;
-		height: auto;
-		margin-top: 3em;
-		margin-bottom: 24px;
-	}
+  @media ${device.mobile} {
+    width: 320px;
+    height: auto;
+    margin-top: 3em;
+    margin-bottom: 24px;
+  }
 `;
 
 const StatsContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: start;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
 
-	@media ${device.mobile} {
-		align-items: flex-start;
-		width: 30%;
-	}
+  @media ${device.mobile} {
+    align-items: flex-start;
+    width: 30%;
+  }
 `;
 
 const Number = styled.div`
-	font-weight: 700;
-	font-size: 40px;
-	line-height: 48px;
-	color: #33364c;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 48px;
+  color: #33364c;
 
-	@media ${device.mobile} {
-		font-size: 20px;
-		line-height: 24px;
-	}
+  @media ${device.mobile} {
+    font-size: 20px;
+    line-height: 24px;
+  }
 `;
 
 const Line = styled.span`
-	color: #000;
-	font-size: 20px;
-	margin-right: 10px;
+  color: #000;
+  font-size: 20px;
+  margin-right: 10px;
 `;
 
 const About = styled.div`
-	font-weight: 400;
-	font-size: 18px;
-	line-height: 22px;
-	color: #484b5f;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 22px;
+  color: #484b5f;
 
-	@media ${device.mobile} {
-		font-size: 12px;
-	}
+  @media ${device.mobile} {
+    font-size: 12px;
+  }
 `;
 
 const LeftSection = () => {
-	return (
-		<Container>
-			<HeaderTag>
-				<Trade>Trade</Trade>
-				<On>On!</On>
-			</HeaderTag>
-			<Description>
-				TradingLeagues is a first-of-its-kind fantasy trading platform providing
-				a gamified experience of the financial markets.
-			</Description>
-			<GetLinkArea>
-				<InputArea>
-					<NumberSpan>+ 91 </NumberSpan>
-					<Line>|</Line>
-					<Input></Input>
-				</InputArea>
-				<LinkButton>GET DOWNLOAD LINK</LinkButton>
-			</GetLinkArea>
-			<StatsArea>
-				<StatsContainer>
-					<Number>12.2K+</Number>
-					<About>Total Downloads</About>
-				</StatsContainer>
-				<StatsContainer>
-					<Number>12,000</Number>
-					<About>Total Winnings</About>
-				</StatsContainer>
-				<StatsContainer>
-					<Number>12,302</Number>
-					<About>Total Downloads</About>
-				</StatsContainer>
-			</StatsArea>
-		</Container>
-	);
+  return (
+    <Container>
+      <HeaderTag>
+        <AnimContainer>
+          <AnimText>
+            <span>Game</span>
+            <span>Trade</span>
+            <span>Game</span>
+          </AnimText>
+        </AnimContainer>
+        <span>On!</span>
+      </HeaderTag>
+      <Description>
+        TradingLeagues is a first-of-its-kind fantasy trading platform providing
+        a gamified experience of the financial markets.
+      </Description>
+      <GetLinkArea>
+        <InputArea>
+          <NumberSpan>+ 91 </NumberSpan>
+          <Line>|</Line>
+          <Input></Input>
+        </InputArea>
+        <LinkButton>GET DOWNLOAD LINK</LinkButton>
+      </GetLinkArea>
+      <StatsArea>
+        <StatsContainer>
+          <Number>12.2K+</Number>
+          <About>Total Downloads</About>
+        </StatsContainer>
+        <StatsContainer>
+          <Number>12,000</Number>
+          <About>Total Winnings</About>
+        </StatsContainer>
+        <StatsContainer>
+          <Number>12,302</Number>
+          <About>Total Downloads</About>
+        </StatsContainer>
+      </StatsArea>
+    </Container>
+  );
 };
 
 const HeroContainer = styled.div`
-	padding: 2rem;
+  padding: 2rem;
 
-	@media ${device.mobile} {
-		padding: 0.5rem;
-		width: 100vw;
-		margin-bottom: 13em;
-	}
+  @media ${device.mobile} {
+    padding: 0.5rem;
+    width: 100vw;
+    margin-bottom: 13em;
+  }
 `;
 
 const SytledSection = styled.div`
-	background-color: #00df8d;
-	border-radius: 40px;
-	padding: 3em;
-	padding-bottom: 7.5em;
-	box-sizing: border-box;
+  background-color: #00df8d;
+  border-radius: 40px;
+  padding: 3em;
+  padding-bottom: 7.5em;
+  box-sizing: border-box;
 
-	@media ${device.mobile} {
-		padding: 1.5em 1em;
-		padding-bottom: 7.5em;
-	}
+  @media ${device.mobile} {
+    padding: 1.5em 1em;
+    padding-bottom: 7.5em;
+  }
 `;
 
 const HeroContentSection = styled.div`
-	display: flex;
-	flex-direction: row;
-	justify-content: space-between;
-	width: 80%;
-	margin: 0 auto;
-	margin-top: 4em;
-	position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 80%;
+  margin: 0 auto;
+  margin-top: 4em;
+  position: relative;
 
-	@media ${device.mobile} {
-		display: block;
-		margin: 0;
-		width: auto;
-		height: 700px;
-	}
+  @media ${device.mobile} {
+    display: block;
+    margin: 0;
+    width: auto;
+    height: 700px;
+  }
 `;
 
 const ImageArea = styled.div`
-	@media ${device.mobile} {
-		/* top: auto; */
-		/* bottom: -15.5em; */
-		width: 80%;
-		margin: 0 10%;
-		transform: translateY(50%);
-		margin-top: -10rem;
-	}
+  @media ${device.mobile} {
+    /* top: auto; */
+    /* bottom: -15.5em; */
+    width: 80%;
+    margin: 0 10%;
+    transform: translateY(50%);
+    margin-top: -10rem;
+  }
 
-	/* & img {
+  /* & img {
 		@media ${device.mobile} {
 			width: 100%;
 			height: auto;
@@ -272,58 +314,58 @@ const ImageArea = styled.div`
 `;
 
 const StyledVideo = styled.video`
-	height: 588px;
-	width: 303px;
-	aspect-ratio: 5 / 11;
-	/* display: block; */
-	/* opacity: 0; */
-	transition: opacity 200ms ease-in-out;
-	border-radius: 15px;
-	position: absolute;
-	bottom: 40;
-	right: 0;
-	/* border: 1px solid red; */
+  height: 588px;
+  width: 303px;
+  aspect-ratio: 5 / 11;
+  /* display: block; */
+  /* opacity: 0; */
+  transition: opacity 200ms ease-in-out;
+  border-radius: 15px;
+  position: absolute;
+  bottom: 40;
+  right: 0;
+  /* border: 1px solid red; */
 
-	@media ${device.mobile} {
-		border-radius: 30px;
-		/* position: relative; */
-		width: 70%;
-		height: auto;
-		margin-bottom: 1.5em;
-		opacity: 1;
-		height: 588px;
-		width: 303px;
-	}
+  @media ${device.mobile} {
+    border-radius: 30px;
+    /* position: relative; */
+    width: 70%;
+    height: auto;
+    margin-bottom: 1.5em;
+    opacity: 1;
+    height: 588px;
+    width: 303px;
+  }
 
-	&.active {
-		opacity: 1;
+  &.active {
+    opacity: 1;
 
-		&.exiting {
-			opacity: 0;
-		}
-	}
+    &.exiting {
+      opacity: 0;
+    }
+  }
 `;
 
 const Hero = () => {
-	return (
-		<HeroContainer>
-			<SytledSection>
-				<NavBar />
-				<HeroContentSection>
-					<LeftSection />
-					<ImageArea>
-						<StyledVideo
-							muted
-							autoPlay={true}
-							loop
-							// playsInline
-							src={`${asset_prefix}/assets/HomePage/Herocard.mp4`}
-						/>
-					</ImageArea>
-				</HeroContentSection>
-			</SytledSection>
-		</HeroContainer>
-	);
+  return (
+    <HeroContainer>
+      <SytledSection>
+        <NavBar />
+        <HeroContentSection>
+          <LeftSection />
+          <ImageArea>
+            <StyledVideo
+              muted
+              autoPlay={true}
+              loop
+              // playsInline
+              src={`${asset_prefix}/assets/HomePage/Herocard.mp4`}
+            />
+          </ImageArea>
+        </HeroContentSection>
+      </SytledSection>
+    </HeroContainer>
+  );
 };
 
 export default Hero;
--- components/Home/Swiper.tsx
+++ components/Home/Swiper.tsx
@@ -9,331 +9,331 @@ import device from "../../styles/breakpoints";
 const asset_prefix = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 const StyledSection = styled.section`
-	background-color: #fff;
-	padding: 6em 0;
+  background-color: #ffffff;
+  padding: 6em 0;
 
-	@media ${device.mobile} {
-		padding: 3em 0;
-	}
+  @media ${device.mobile} {
+    padding: 3em 0;
+  }
 `;
 
 const SectionContent = styled.div`
-	margin: 0 auto;
-	width: min(70vw, 1200px);
-	color: #15182c;
-	padding: 0 3rem;
+  margin: 0 auto;
+  width: min(70vw, 1200px);
+  color: #15182c;
+  padding: 0 3rem;
 
-	@media ${device.mobile} {
-		width: 90vw;
-		padding: 0;
-	}
+  @media ${device.mobile} {
+    width: 90vw;
+    padding: 0;
+  }
 `;
 
 const BigHeading = styled.h2`
-	color: #15182c;
-	font-size: 4rem;
-	font-weight: 700;
-	line-height: 76px;
-	margin-bottom: 0.5em;
+  color: #15182c;
+  font-size: 4rem;
+  font-weight: 700;
+  line-height: 76px;
+  margin-bottom: 0.5em;
 
-	@media ${device.mobile} {
-		font-size: 40px;
-		line-height: 48px;
-	}
+  @media ${device.mobile} {
+    font-size: 40px;
+    line-height: 48px;
+  }
 `;
 
 const StyledDescription = styled.p`
-	font-size: 1.5rem;
-	font-weight: 400;
-	line-height: 33.6px;
-	max-width: 480px;
-	margin-bottom: 2em;
+  font-size: 1.5rem;
+  font-weight: 400;
+  line-height: 33.6px;
+  max-width: 480px;
+  margin-bottom: 2em;
 
-	@media ${device.mobile} {
-		font-size: 1rem;
-		font-weight: 400;
-		line-height: 22px;
-	}
+  @media ${device.mobile} {
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 22px;
+  }
 `;
 
 const TabLinks = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	gap: 2.5em;
-	@media ${device.mobile} {
-		display: none;
-	}
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 2.5em;
+  @media ${device.mobile} {
+    display: none;
+  }
 `;
 
 const TabLinkItem = styled.button<{ "data-active": boolean }>`
-	font-size: 1rem;
-	font-weight: 700;
-	background: none;
-	border: none;
-	font-family: inherit;
-	color: ${(props) => (props["data-active"] ? "#00DF8D" : "#737585")};
-	text-transform: uppercase;
-	cursor: pointer;
-	margin-bottom: 6em;
-	@media ${device.mobile} {
-	}
+  font-size: 1rem;
+  font-weight: 700;
+  background: none;
+  border: none;
+  font-family: inherit;
+  color: ${(props) => (props["data-active"] ? "#00DF8D" : "#737585")};
+  text-transform: uppercase;
+  cursor: pointer;
+  margin-bottom: 6em;
+  @media ${device.mobile} {
+  }
 `;
 
 const LeagueContentMobile = styled.div`
-	padding: 0;
-	max-height: 0;
-	overflow: hidden;
+  padding: 0;
+  max-height: 0;
+  overflow: hidden;
 
-	&:not(:last-child) {
-		margin-bottom: 3em;
-	}
+  &:not(:last-child) {
+    margin-bottom: 3em;
+  }
 
-	@media ${device.mobile} {
-		padding-left: 0.5em;
-	}
+  @media ${device.mobile} {
+    padding-left: 0.5em;
+  }
 `;
 
 const TabLinkItemMobile = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	padding: 0.5em 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.5em 0;
 
-	& h2 {
-		font-size: 1rem;
-		font-weight: 700;
-		text-transform: uppercase;
-		color: #737585;
-	}
+  & h2 {
+    font-size: 1rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: #737585;
+  }
 
-	& button {
-		background: none;
-		border: none;
-	}
+  & button {
+    background: none;
+    border: none;
+  }
 
-	&.active {
-		& h2 {
-			color: #00df8d;
-		}
+  &.active {
+    & h2 {
+      color: #00df8d;
+    }
 
-		&.active + div {
-			padding: 1em 0;
-			padding-left: 0.5em;
-			max-height: none;
-		}
-	}
+    &.active + div {
+      padding: 1em 0;
+      padding-left: 0.5em;
+      max-height: none;
+    }
+  }
 `;
 
 const LeagueMobile = styled.div``;
 
 const MainContent = styled.div`
-	display: grid;
-	grid-template-columns: repeat(12, 1fr);
-	column-gap: 1em;
-	margin-top: 10px;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  column-gap: 1em;
+  margin-top: 10px;
 
-	@media ${device.mobile} {
-		display: none;
-		visibility: hidden;
-	}
+  @media ${device.mobile} {
+    display: none;
+    visibility: hidden;
+  }
 `;
 
 const MainContentMobile = styled.div`
-	display: none;
-	scroll-snap-align: end;
+  display: none;
+  scroll-snap-align: end;
 
-	@media ${device.mobile} {
-		display: block;
-	}
+  @media ${device.mobile} {
+    display: block;
+  }
 `;
 
 const LeftSection = styled.div`
-	grid-column: 1 / 7;
-	opacity: 1;
-	transition: opacity 200ms ease-in;
+  grid-column: 1 / 7;
+  opacity: 1;
+  transition: opacity 200ms ease-in;
 
-	&.exiting {
-		opacity: 0;
+  &.exiting {
+    opacity: 0;
 
-		& img {
-			opacity: 0;
-		}
-	}
+    & img {
+      opacity: 0;
+    }
+  }
 
-	&.entering {
-		opacity: 1;
+  &.entering {
+    opacity: 1;
 
-		& img {
-			opacity: 1;
-		}
-	}
+    & img {
+      opacity: 1;
+    }
+  }
 `;
 
 const RightSection = styled.div`
-	grid-column: 7 / 13;
-	justify-self: end;
-	position: relative;
-	align-self: center;
-	width: 318px;
-	height: 446px;
+  grid-column: 7 / 13;
+  justify-self: end;
+  position: relative;
+  align-self: center;
+  width: 318px;
+  height: 446px;
 `;
 
 const StyledHeading = styled.h2`
-	color: ${(props) => props.color};
-	font-size: 40px;
-	font-weight: 700;
-	margin-bottom: 0.5em;
+  color: ${(props) => props.color};
+  font-size: 40px;
+  font-weight: 700;
+  margin-bottom: 0.5em;
 
-	@media ${device.mobile} {
-		font-size: 32px;
-	}
+  @media ${device.mobile} {
+    font-size: 32px;
+  }
 `;
 
 const LeagueDescription = styled.p`
-	font-size: 36px;
-	font-weight: 600;
-	line-height: 50.4px;
-	width: 468px;
+  font-size: 36px;
+  font-weight: 600;
+  line-height: 50.4px;
+  width: 468px;
 
-	@media ${device.mobile} {
-		font-size: 24px;
-		width: 312px;
-		line-height: 33.6px;
-		margin-bottom: 1em;
-	}
+  @media ${device.mobile} {
+    font-size: 24px;
+    width: 312px;
+    line-height: 33.6px;
+    margin-bottom: 1em;
+  }
 `;
 
 const StyledVideo = styled.video`
-	height: 500px;
-	/* width: 400px; */
-	/* width: 100%; */
-	aspect-ratio: 5 / 11;
-	display: block;
-	opacity: 0;
-	transition: opacity 200ms ease-in-out;
-	/* border-radius: 15px; */
-	position: absolute;
-	bottom: 0;
-	right: 0;
+  height: 500px;
+  /* width: 400px; */
+  /* width: 100%; */
+  aspect-ratio: 5 / 11;
+  display: block;
+  opacity: 0;
+  transition: opacity 200ms ease-in-out;
+  /* border-radius: 15px; */
+  position: absolute;
+  bottom: 0;
+  right: 0;
 
-	@media ${device.mobile} {
-		border-radius: 30px;
-		position: relative;
-		width: 70%;
-		height: 525px;
-		margin-bottom: 1.5em;
-		opacity: 1;
-	}
+  @media ${device.mobile} {
+    border-radius: 30px;
+    position: relative;
+    width: 70%;
+    height: 525px;
+    margin-bottom: 1.5em;
+    opacity: 1;
+  }
 
-	&.active {
-		opacity: 1;
+  &.active {
+    opacity: 1;
 
-		&.exiting {
-			opacity: 0;
-		}
-	}
+    &.exiting {
+      opacity: 0;
+    }
+  }
 `;
 
 const Swiper: () => JSX.Element = () => {
-	const [currentTab, setCurrentTab] = useState<number>(0);
-	const [leagueDetails, setLeagueDetails] = useState<LeagueDetailsType>(
-		leagueDetailsData[currentTab],
-	);
+  const [currentTab, setCurrentTab] = useState<number>(0);
+  const [leagueDetails, setLeagueDetails] = useState<LeagueDetailsType>(
+    leagueDetailsData[currentTab]
+  );
 
-	useEffect(() => {
-		setLeagueDetails({ ...leagueDetails, state: "exiting" });
-		setTimeout(() => {
-			setLeagueDetails({ ...leagueDetailsData[currentTab], state: "entering" });
-		}, 200);
-	}, [currentTab]);
+  useEffect(() => {
+    setLeagueDetails({ ...leagueDetails, state: "exiting" });
+    setTimeout(() => {
+      setLeagueDetails({ ...leagueDetailsData[currentTab], state: "entering" });
+    }, 200);
+  }, [currentTab]);
 
-	return (
-		<StyledSection>
-			<SectionContent>
-				<BigHeading>Get your trade game on!</BigHeading>
-				<StyledDescription>
-					Choose from four game formats to get an immersive experience of the
-					financial markets
-				</StyledDescription>
-				<TabLinks>
-					{tabLinksData.map((x, i) => (
-						<TabLinkItem
-							key={x.key}
-							data-active={currentTab === i}
-							onClick={() => setCurrentTab(i)}
-						>
-							{x.title}
-						</TabLinkItem>
-					))}
-				</TabLinks>
-				{leagueDetails && (
-					<MainContent>
-						<LeftSection className={leagueDetails.state}>
-							<StyledHeading color={leagueDetails.color}>
-								{leagueDetails.heading}
-							</StyledHeading>
-							<LeagueDescription>{leagueDetails.para}</LeagueDescription>
-						</LeftSection>
-						<RightSection>
-							{leagueDetailsData.map((x, i) => (
-								<StyledVideo
-									color={x.color}
-									className={`${
-										currentTab === i ? "active " + leagueDetails.state : ""
-									}`}
-									key={x.key}
-									muted
-									autoPlay={true}
-									loop
-									// playsInline
-									src={`${asset_prefix}/assets/swiper/${x.vid_file}.mp4`}
-								/>
-							))}
-						</RightSection>
-					</MainContent>
-				)}
-				<MainContentMobile>
-					{leagueDetailsData.map((x, i) => (
-						<LeagueMobile key={x.key}>
-							<TabLinkItemMobile className={currentTab === i ? "active" : ""}>
-								<h2>{x.heading}</h2>
-								<button
-									onClick={(ev) => {
-										let setIndex = i;
-										if (currentTab === i) setIndex = -1;
-										setCurrentTab(setIndex);
-										setTimeout(() => {
-											(ev.target as HTMLElement).scrollIntoView({
-												behavior: "smooth",
-											});
-										}, 200);
-									}}
-								>
-									<img
-										src={`${asset_prefix}/assets/swiper/arrow-${
-											currentTab === i ? "up" : "down"
-										}.svg`}
-									/>
-								</button>
-							</TabLinkItemMobile>
-							<LeagueContentMobile>
-								<StyledHeading color={x.color}>{x.heading}</StyledHeading>
-								<LeagueDescription>{leagueDetails.para}</LeagueDescription>
-								<StyledVideo
-									muted
-									autoPlay={true}
-									loop
-									// playsInline
-									src={`${asset_prefix}/assets/swiper/${x.vid_file}.mp4`}
-								/>
-							</LeagueContentMobile>
-						</LeagueMobile>
-					))}
-				</MainContentMobile>
-			</SectionContent>
-		</StyledSection>
-	);
+  return (
+    <StyledSection>
+      <SectionContent>
+        <BigHeading>Get your trade game on!</BigHeading>
+        <StyledDescription>
+          Choose from four game formats to get an immersive experience of the
+          financial markets
+        </StyledDescription>
+        <TabLinks>
+          {tabLinksData.map((x, i) => (
+            <TabLinkItem
+              key={x.key}
+              data-active={currentTab === i}
+              onClick={() => setCurrentTab(i)}
+            >
+              {x.title}
+            </TabLinkItem>
+          ))}
+        </TabLinks>
+        {leagueDetails && (
+          <MainContent>
+            <LeftSection className={leagueDetails.state}>
+              <StyledHeading color={leagueDetails.color}>
+                {leagueDetails.heading}
+              </StyledHeading>
+              <LeagueDescription>{leagueDetails.para}</LeagueDescription>
+            </LeftSection>
+            <RightSection>
+              {leagueDetailsData.map((x, i) => (
+                <StyledVideo
+                  color={x.color}
+                  className={`${
+                    currentTab === i ? "active " + leagueDetails.state : ""
+                  }`}
+                  key={x.key}
+                  muted
+                  autoPlay={true}
+                  loop
+                  // playsInline
+                  src={`${asset_prefix}/assets/swiper/${x.vid_file}.mp4`}
+                />
+              ))}
+            </RightSection>
+          </MainContent>
+        )}
+        <MainContentMobile>
+          {leagueDetailsData.map((x, i) => (
+            <LeagueMobile key={x.key}>
+              <TabLinkItemMobile className={currentTab === i ? "active" : ""}>
+                <h2>{x.heading}</h2>
+                <button
+                  onClick={(ev) => {
+                    let setIndex = i;
+                    if (currentTab === i) setIndex = -1;
+                    setCurrentTab(setIndex);
+                    setTimeout(() => {
+                      (ev.target as HTMLElement).scrollIntoView({
+                        behavior: "smooth",
+                      });
+                    }, 200);
+                  }}
+                >
+                  <img
+                    src={`${asset_prefix}/assets/swiper/arrow-${
+                      currentTab === i ? "up" : "down"
+                    }.svg`}
+                  />
+                </button>
+              </TabLinkItemMobile>
+              <LeagueContentMobile>
+                <StyledHeading color={x.color}>{x.heading}</StyledHeading>
+                <LeagueDescription>{leagueDetails.para}</LeagueDescription>
+                <StyledVideo
+                  muted
+                  autoPlay={true}
+                  loop
+                  // playsInline
+                  src={`${asset_prefix}/assets/swiper/${x.vid_file}.mp4`}
+                />
+              </LeagueContentMobile>
+            </LeagueMobile>
+          ))}
+        </MainContentMobile>
+      </SectionContent>
+    </StyledSection>
+  );
 };
 
 export default Swiper;
