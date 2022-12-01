b19aadfd6b754eb6bfb1e23b71ba3937b7b31d41
Vikash <vikash@rainplatforms.com>
2022-12-01 16:45:09 +0530

    added links and testimonials

--- lib/social.ts
+++ lib/social.ts
@@ -1,34 +1,36 @@
+/** @format */
+
 import { SocialLinksType } from "../types";
 
 export const socialLinksData: SocialLinksType[] = [
-  {
-    title: "Twitter",
-    key: "twitter",
-    url: "https://wwww.twitter.com",
-    img: "Twitter",
-  },
-  {
-    title: "Facebook",
-    key: "fb",
-    url: "https://wwww.twitter.com",
-    img: "FB",
-  },
-  {
-    title: "Instagram",
-    key: "insta",
-    url: "https://wwww.twitter.com",
-    img: "Instagram",
-  },
-  {
-    title: "LinkedIn",
-    key: "linkedin",
-    url: "https://wwww.twitter.com",
-    img: "Linkedin",
-  },
-  {
-    title: "YouTube",
-    key: "yt",
-    url: "https://wwww.twitter.com",
-    img: "YouTube",
-  },
+	{
+		title: "Twitter",
+		key: "twitter",
+		url: "https://twitter.com/trading_leagues",
+		img: "Twitter",
+	},
+	{
+		title: "Facebook",
+		key: "fb",
+		url: "https://www.facebook.com/tradingleagues",
+		img: "FB",
+	},
+	{
+		title: "Instagram",
+		key: "insta",
+		url: "https://www.instagram.com/tradingleagueshq/",
+		img: "Instagram",
+	},
+	{
+		title: "LinkedIn",
+		key: "linkedin",
+		url: "https://www.linkedin.com/showcase/tradingleagues/",
+		img: "Linkedin",
+	},
+	// {
+	// 	title: "YouTube",
+	// 	key: "yt",
+	// 	url: "https://wwww.twitter.com",
+	// 	img: "YouTube",
+	// },
 ];
--- lib/testimonials.ts
+++ lib/testimonials.ts
@@ -1,25 +1,27 @@
+/** @format */
+
 import { TestimonialType } from "../types";
 
 export const testimonialsData: TestimonialType[] = [
-  {
-    text: "I love playing TradingLeagues, it is so addictive and fun! I can play it for free and earn money everyday!",
-    name: "Ankur Desai",
-    img: "AnkurDesai",
-    amount: "12 Lakh",
-    "grid-area": "a",
-  },
-  {
-    text: "I love playing TradingLeagues, it is so addictive and fun! I can play it for free and earn money everyday!",
-    name: "Jeevan Kumar",
-    img: "JeevanKumar",
-    amount: "12 Lakh",
-    "grid-area": "b",
-  },
-  {
-    text: "I love playing TradingLeagues, it is so addictive and fun! I can play it for free and earn money everyday!",
-    name: "Rachna Singh",
-    img: "RachnaSingh",
-    amount: "12 Lakh",
-    "grid-area": "c",
-  },
+	{
+		text: "I love playing TradingLeagues, it is so addictive and fun! I can play it for free and earn money everyday!",
+		name: "Ankur Desai",
+		img: "AnkurDesai",
+		amount: "12 Lakh",
+		"grid-area": "a",
+	},
+	{
+		text: "Being part of the beta program gave me first-hand experience of what TradingLeagues is trying to achieve. It's truly remarkable how simple and effective TradingLeagues has made my experience of the markets. No brokerage, No capital; click a few buttons, and I get to play, learn and earn.",
+		name: "Rachna Singh",
+		img: "RachnaSingh",
+		amount: "12K",
+		"grid-area": "c",
+	},
+	{
+		text: " This app is brilliant! Every game prepares you for the stock market and it is fun! You can win real money and even play with your friends here!",
+		name: "Jeevan Kumar",
+		img: "JeevanKumar",
+		amount: "32K",
+		"grid-area": "b",
+	},
 ];
